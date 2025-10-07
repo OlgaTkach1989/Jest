50d559cedca605201ec92ef50ea21e333ef70510
OlgaTkach1989 <olga.tkachuk378@gmail.com>
2025-10-07 11:41:34 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules", "coverage", "dist", "build"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module", // ES6
+      globals: {
+        ...globals.node, // erlaubt require, module, etc.
+      },
+    },
+    ...js.configs.recommended,
+  },
+  {
+    // spezielle Regeln/Umgebung für Tests
+    files: [
+      "**/*.test.js",
+      "**/*.spec.js",
+      "**/__tests__/**/*.js" // 
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest, // erlaubt describe, test, expect, etc.
+      },
+    },
+  },
+];
